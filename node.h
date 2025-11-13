23cfe6646b542dbe9c3ac9fbed67e670c6bd2077
z-mdrzz <alilouzaid7@gmail.com>
2025-11-13 14:57:12 +0100

    Update node.h

--- node.h
+++ node.h
@@ -1,3 +1,4 @@
+//construye listas enlazadas, cada nodo tiene (T,puntero siguiente)
 #include <ostream>
 using namespace std;
 template <typename T> 
@@ -5,14 +6,15 @@ class Node {
     public:
 	//atributos
         T data;
-	Node<T>* next;
-	//métodos
-	Node(T data, Node<T> next=nullptr){
-		this->data = data;
+	Node<T>* next;// puntero al suiguiente nodo
+// constructor
+Node(T data, Node<T> next=nullptr){
+		this->data = data;// guarda en data y lo enlaza con el nodo
 		this->next = next;
 	}
+//impresor
 	friend ostream& operator<<(std::ostream &out, const Node<T> &node){
-		out << node.data;
+		out << node.data;//solo el dato
 		return out;
 	}
     
